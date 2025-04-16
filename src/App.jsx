7612f97978b98e7d9cf1c65a45f708e4fe24ed59
Nama Kamu <nizarrarfan@gmail.com>
2025-04-16 21:51:35 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import { useState } from "react";
-import { useGetAnime } from "./hooks/useGetAnime";
 import { Route, Router, Routes } from "react-router-dom";
-import DetailAnimePage from "./components/pages/detailAnimePage";
+import DetailAnimePage from "./components/pages/DetailAnimePage";
 import CompletedAnime from "./components/layout/CompletedAnime";
 import HomePage from "./components/pages/HomePage";
 import StreamingPage from "./components/pages/StreamingPages";
 import DownloadPages from "./components/pages/DownloadPages";
 import AboutPage from "./components/pages/AboutPages";
 
-
 function App() {
   return (
-   
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/home" element={<HomePage />}></Route>
-        <Route path="/detail/:animeId" element={<DetailAnimePage />}></Route>
-        <Route path="/streaming/:episodeId" element={<StreamingPage />}></Route>
-        <Route path="/batch/:batchId" element={<DownloadPages />}></Route>
-        <Route path="/anime" element={<CompletedAnime />}></Route>
-        <Route path="/about" element={<AboutPage />}></Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/home" element={<HomePage />}></Route>
+      <Route path="/detail/:animeId" element={<DetailAnimePage />}></Route>
+      <Route path="/streaming/:episodeId" element={<StreamingPage />}></Route>
+      <Route path="/batch/:batchId" element={<DownloadPages />}></Route>
+      <Route path="/anime" element={<CompletedAnime />}></Route>
+      <Route path="/about" element={<AboutPage />}></Route>
+    </Routes>
   );
 }
 
